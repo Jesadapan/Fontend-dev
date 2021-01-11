b7fec72de9f6ae856dc87d528e2f0733e64cacd3
Jesadapan <71056687+Jesadapan@users.noreply.github.com>
2021-01-11 16:48:53 +0700

    add

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <script src="Js/bootstrap.min.js"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
     
+    <link rel="stylesheet" href="icon/css/all.css">
     <link rel="stylesheet" href="css/mystyle.css">
     <script src="Js/defualt.js"></script>
     
@@ -15,7 +16,8 @@
 <body>
     <div class="container">
         <img src="img/90158301_2568754700060299_7244456307510476800_o.jpg" alt="">
-        <button class="btn btn-primary"> xxxx </button>
+        
+        <button class="btn btn-primary"><i class="fab fa-accessible-icon"></i> xxxx </button>
         <button class="btn btn-primary"> xxx </button>
     </div>
     <script>
